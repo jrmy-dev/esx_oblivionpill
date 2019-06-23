f9c19720b1fa25a3a1a93046f377dd5a1a1f602a
Jérémy <50837004+jrmycsm@users.noreply.github.com>
2019-06-23 08:34:32 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,12 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
 locksound = false
 local pillused = true
 
